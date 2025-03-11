1cfca7ada987d0e6fe05ce9adef651c2fe8a8556
Cesar Santos <csar.santos18@gmail.com>
2025-03-11 15:44:43 -0500

    contador

--- minha_app/views.py
+++ minha_app/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 async def contador_de_tempo(request):
     for i in range(5):
         print(f"Contagem: {i}")
-        await asyncio.sleep(1)  # espera 1 segundo de forma assíncrona
+        await asyncio.sleep(1)
 
     return HttpResponse("Contagem concluída!")
 
